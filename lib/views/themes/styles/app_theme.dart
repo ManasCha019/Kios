89553f8ee6e103e7e3c563df2a0b30f71a5a4745
Manas <152583426+ManasCha019@users.noreply.github.com>
2025-02-03 19:50:34 +0700

    first commit

---
+++ lib/views/themes/styles/app_theme.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static const TextStyle displayMedium = TextStyle(
+    fontSize: 20.0,
+    fontWeight: FontWeight.bold,
+  );
+  static const TextStyle displaySmall = TextStyle(
+    fontSize: 12.0,
+    fontWeight: FontWeight.w400,
+  );
+}
---
+++ lib/views/widgets/colors/basicflutter_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Kios_colorsColors {
+  Kios_colorsColors._();
+  static const MaterialColor green = MaterialColor(
+    _greenPrimaryValue,
+    <int, Color>{50: Color(_greenPrimaryValue)},
+  );
+  static const int _greenPrimaryValue = 0xFF88d049;
+}
